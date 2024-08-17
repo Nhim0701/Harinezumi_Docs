18838ef0e3a1107f0a2afd20c72ffccbb968414f
Nhim0701 <hieunguyen.hari0701@gmail.com>
2024-08-17 09:37:52 +0700

    edit index

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,40 +4,37 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
+    title: 'Who am i',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Chào bạn! Mình là tên Hiếu, người đam mê công nghệ và thích chia sẻ kiến thức qua blog. 
+        Xuất phát điểm của mình làm role IT Comtor nên thực ra mình không biết nhiều về tech.
+        Sau hơn 2 năm làm việc trong ngành này mình quyết định dấn thân vào việc học về tech nhiều hơn
+        cụ thể là về devops. Mình sẽ chia sẻ những gì mình học, kinh nghiệm , trải nghiệm lên Blog này.
+        Nếu bạn thấy Blog này có ích hãy cho mình biết tại địa chỉ mail này nhé  <a href="mailto:hieunguyen.hari0701@gmail.com">hieunguyen.hari0701@gmail.com</a>
+      </>
+    ),
+  },
+  {
+    title: 'Mình là WIBU',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Mình lấy nhân vật anime làm hình nên thì phần nào các bạn cũng đoán được, <code>Mình là WIBU</code>
+        Mình rất thích nhân vật zenitsu này, bình thường nhút nhát, sợ hãi nhưng tốt bụng, và lúc cần nghiêm túc 
+        thì bao ngầu.Bản thân mình bước chân vào ngành IT cũng vậy cái gì cũng sợ , thứ mình duy nhất tự tin là tiếng Nhật
+        giờ đây mình sẽ phải nghiêm túc mode học thêm nhiều thứ trong ngành này. Các bạn đồng hành cùng mình nhé
       </>
     ),
   },
 ];
 
+
+
 function Feature({Svg, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--6 ')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
